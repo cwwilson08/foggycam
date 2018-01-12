f3b909ca4e62651afc712526d1b873282be271d5
Den Delimarsky <ddelimarsky@live.com>
2018-01-11 23:54:11 -0800

    :package: More config updates.

--- start.py
+++ start.py
@@ -8,4 +8,4 @@ print 'Welcome to FoggyCam 1.0 - Nest video/image capture tool'
 CONFIG = json.load(open('config.json'))
 
 CAM = FoggyCam(username=CONFIG['username'], password=CONFIG['password'])
-CAM.capture_images(produce_video=True, clear_images=True, custom_path=CONFIG['path'], frame_rate=CONFIG['frame_rate'], width=CONFIG['width'])
+CAM.capture_images(clear_images=bool(CONFIG['clear_images']), produce_video=bool(CONFIG['produce_video']), custom_path=CONFIG['path'], frame_rate=CONFIG['frame_rate'], width=CONFIG['width'])
